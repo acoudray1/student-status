db6dfa0ba45c7ada076818ccd975658ec7beebce
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-28 10:16:52 +0100

    status component creation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import StatusComponent from './main/StatusComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      </header>
+      <main className="App-body">
+        <StatusComponent />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/StatusComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './StatusComponent.module.css';
+
+const StatusComponent = (props) => {
+    return (
+        <div className={styles.statuscard}>
+
+        </div>
+    );
+};
+
+export default StatusComponent;
